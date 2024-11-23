18909b9859a5231a2e0dda01133286e964edbbbe
TECHATHAM KANJANASUTTAKUL <techatham.neng@mail.kmutt.ac.th>
2024-11-23 13:40:23 +0700

    update

--- Edugo_web/src/router/AppRouter.jsx
+++ Edugo_web/src/router/AppRouter.jsx
@@ -6,7 +6,7 @@ import Homepage from '../components/Homepage'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Navigate to="/homepage" />
+    element: <Navigate to="/homepage" replace/>
   },
   {
     path: '/homepage',
