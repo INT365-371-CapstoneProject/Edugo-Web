61629c32cdb6e617ef03a7f59c0944db39bd6464
TECHATHAM KANJANASUTTAKUL <techatham.neng@mail.kmutt.ac.th>
2024-11-23 13:46:09 +0700

    Update vite.config.js

--- Edugo_web/vite.config.js
+++ Edugo_web/vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url'; // นำเข้า fileURLToPath
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)), // กำหนด alias ให้สามารถใช้ @ แทน src ได้
     },
-  }
-})
+  },
+});
